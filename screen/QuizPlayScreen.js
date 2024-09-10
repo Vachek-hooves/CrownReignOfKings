7d381607b10643301b371b2a5828bd793d0db527
Vachek-hooves <vachek0602@gmail.com>
2024-09-10 17:15:41 +0300

    update: GameScreen

--- screen/QuizPlayScreen.js
+++ screen/QuizPlayScreen.js
@@ -1,8 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import {MainImageLayout} from '../components/Layout';
 import {IconReturnSword} from '../components/icons';
+import {useCrownQuiz} from '../store/crown_store';
 
 const QuizPlayScreen = () => {
+  const {crownQuiz} = useCrownQuiz();
   return (
     <MainImageLayout>
       <IconReturnSword />
