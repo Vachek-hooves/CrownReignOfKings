eef26da1c200c8ed3c8928b68f39a51eb821bff3
Viacheslav <111743659+Vachek-hooves@users.noreply.github.com>
2024-09-10 11:39:04 +0300

    updateCrown Museum Screen

--- screen/CrownMuseumScreen.js
+++ screen/CrownMuseumScreen.js
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Modal } from 'react-native';
+import { MUSEUM } from '../data/crown_data';
+// update
+=======
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {MainImageLayout} from '../components/Layout';
+>>>>>>> ad9d645469db4e880158fe9114dc8a17433a11cf
 
 const CrownMuseumScreen = () => {
   return <MainImageLayout></MainImageLayout>;
