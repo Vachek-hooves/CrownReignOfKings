5cbe13fb4aadf33840190205cf9cf3cff2f1a98b
Vachek-hooves <vachek0602@gmail.com>
2024-09-11 08:40:51 +0300

    upate: QuizPlayScreen.js

--- screen/QuizPlayScreen.js
+++ screen/QuizPlayScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -7,6 +7,8 @@ import {
   FlatList,
   ImageBackground,
   Dimensions,
+  Modal,
+  Image,
 } from 'react-native';
 import {MainImageLayout} from '../components/Layout';
 import {COLORS} from '../constant/color';
@@ -18,6 +20,7 @@ const {width, height} = Dimensions.get('window');
 const CARD_HEIGHT = height * 0.3;
 
 const QuizPlayScreen = ({navigation}) => {
+  const [selectedLevel, setSelectedLevel] = useState(null);
   const {crownQuiz} = useCrownQuiz();
   console.log(crownQuiz);
 
@@ -29,9 +32,10 @@ const QuizPlayScreen = ({navigation}) => {
       <TouchableOpacity
         disabled={!item.isActive}
         style={[styles.cardContainer, !item.isActive && styles.disabledCard]}
-        onPress={() =>
-          navigation.navigate('QuizQuestionScreen', {levelId: item.id})
-        }>
+        // onPress={() =>
+        //   navigation.navigate('QuizQuestionScreen', {levelId: item.id})
+        // }
+        onPress={() => setSelectedLevel(item)}>
         <ImageBackground
           source={cardImage}
           style={styles.card}
@@ -45,6 +49,16 @@ const QuizPlayScreen = ({navigation}) => {
         </ImageBackground>
       </TouchableOpacity>
     );
+  };
+
+  const closeModal = () => {
+    setSelectedLevel(null);
+  };
+  const startQuizCall = () => {
+    navigation.navigate('QuizQuestionScreen', {
+      levelId: selectedLevel.id,
+    });
+    setSelectedLevel(null);
   };
 
   return (
@@ -61,6 +75,49 @@ const QuizPlayScreen = ({navigation}) => {
       </View>
       <View style={{height: 50}}></View>
       <IconReturnSword />
+      <Modal
+        visible={selectedLevel !== null}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={closeModal}>
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            {selectedLevel && (
+              <>
+                <Image
+                  source={
+                    QUIZ_CARD_IMAGE.find(img => img.id === selectedLevel.id)
+                      ?.image
+                  }
+                  style={styles.modalImage}
+                />
+                <Text style={styles.modalTitle}>{selectedLevel.name}</Text>
+                <Text style={styles.modalAboutText}>{selectedLevel.about}</Text>
+                <Text style={styles.modalDescription}>
+                  {selectedLevel.description}
+                </Text>
+                <TouchableOpacity
+                  style={styles.modalButton}
+                  onPress={
+                    //   () =>
+                    //   navigation.navigate('QuizQuestionScreen', {
+                    //     levelId: selectedLevel.id,
+                    //   }
+                    // )
+                    startQuizCall
+                  }>
+                  <Text style={styles.modalButtonText}>Start Quiz</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.closeButton}
+                  onPress={closeModal}>
+                  <Text style={styles.closeButtonText}>Close</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+        </View>
+      </Modal>
     </MainImageLayout>
   );
 };
@@ -119,6 +176,61 @@ const styles = StyleSheet.create({
   },
   disabledCard: {
     opacity: 0.5,
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: COLORS.black,
+    borderRadius: 10,
+    padding: 20,
+    width: '80%',
+    alignItems: 'center',
+  },
+  modalImage: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: COLORS.beige,
+    marginBottom: 10,
+  },
+  modalDescription: {
+    fontSize: 16,
+    color: COLORS.beige,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  modalButton: {
+    backgroundColor: COLORS.beige,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  modalButtonText: {
+    color: COLORS.black,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  closeButton: {
+    marginTop: 10,
+  },
+  closeButtonText: {
+    color: COLORS.beige,
+    fontSize: 16,
+  },
+  modalAboutText: {
+    color: COLORS.beige,
+    textAlign: 'center',
+    fontSize: 16,
   },
 });
 
